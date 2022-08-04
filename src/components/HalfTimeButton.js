d171db9fa6e08a041dff778ab745a004fb5fccf2
Arthur <anyanjoarthur@hotmail.com>
2022-08-04 22:58:48 +0100

    Connected API And Insert Data into components

--- src/components/HalfTimeButton.js
+++ src/components/HalfTimeButton.js
@@ -1,13 +1,32 @@
-import React from 'react'; 
+import {useState} from 'react';
 
-const HalfTimeButton = (props) => { 
-  
-  return ( 
-    
-    <button className='addButton' onClick={props.onClick}>{props.text}</button> 
-    
-  ); 
-  
-} 
+const HalfTimeButton = (props) => {
+  const [isShown, setIsShown] = useState(false);
+  const [possesionH, setPossessionH] = useState([]);
+  const [possesionA, setPossessionA] = useState([]);
+  const [shotsH, setShotsH] = useState([]);
+  const [shotsA, setShotsA] = useState([]);
+  const [shotsHTar, setShotsHTar] = useState([]);
+  const [shotsATar, setShotsATar] = useState([]);
+  const [cornerH, setCornersH] = useState([]);
+  const [cornerA, setCornersA] = useState([]);
 
+  const handleClick = event => {
+
+    setIsShown(current => !current);
+  };
+
+  return (
+    <div>
+      <button className="addButton" onClick={handleClick}>{props.text}</button>
+
+      {isShown && (
+        <div>
+          <h2>Some Content</h2>
+          </div>
+      
+      )}
+    </div>
+  );
+}
 export {HalfTimeButton};
